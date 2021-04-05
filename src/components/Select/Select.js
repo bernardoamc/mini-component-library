507aa0a6e6c3435282d5819fdeadb6812877db5e
Bernardo Chaves <bernardo.amc@gmail.com>
2021-04-04 20:36:09 -0400

    Add Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,64 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <OriginalSelect value={value} onChange={onChange}>
+        {children}
+      </OriginalSelect>
+      <SelectUI>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </SelectUI>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const OriginalSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const SelectUI = styled.div`
+  padding: 12px 16px;
+  padding-right: 52px;
+  font-size: 1rem;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+
+  ${OriginalSelect}:focus + & {
+    outline: 1px solid blue;
+  }
+  ${OriginalSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  width: 24px;
+  height: 24px;
+  margin: auto;
+
+  /*
+    The pointer-events CSS property sets under what circumstances (if any) a
+    particular graphic element can become the target of pointer events.
+  */
+  pointer-events: none;
+`;
 
 export default Select;
